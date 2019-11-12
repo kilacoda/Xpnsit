6b41d90b0cf8fbd1607a592bab89bac65362bdc6
Manoj Goel <mgoel@zensoftware.in>
2019-11-12 14:05:55 +0530

    Final commit for this year probs.  Everything working except Analytics to some extent. Also need to just add a Delete function for the History.

--- main_2.py
+++ main_2.py
@@ -1,7 +1,9 @@
 # <------------------------ Imports ------------------------------> #
-# <--- MySQL Errors ---> #
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import numpy as np
+import tkinter as tk
+
+# <--- MySQL Errors ---> #
 import mysql.connector.errors as SQLErrors
 
 # <--- Miscellenaeous ---> #
@@ -29,12 +31,14 @@ else:
 
 # <------------------------- Useful Stuff ---------------------------------> #
 
-tooltips = [
-    'Log a new transaction',
-    'Quickly jot down transactions and make calculations. Export them to a csv file',
-    'View graphs of your expenditure and income history',
-    'See all of your transactions'
-]
+# tooltips = [
+#     'Log a new transaction',
+#     'Quickly jot down transactions and make calculations. Export them to a csv file',
+#     'View graphs of your expenditure and income history',
+#     'See all of your transactions'
+# ]
+
+graph_active = False
 
 heading_format = {
     "justification": 'center',
@@ -49,6 +53,11 @@ day = datetime.datetime.now().day
 months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
           'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 
+months_30 = ['Apr', 'Jun', 'Sep', 'Nov']
+months_31 = [month for month in months if month !=
+             'Feb' and month not in months_30]
+month_name = months[month-1]
+days_in_month = 30 if month_name in months_30 else 31 if month_name in months_31 else 28
 # <----------------------- MySQL -----------------------------> #
 
 '''
@@ -86,7 +95,8 @@ def check_login_info(User: str, Pass: str) -> bool:
     except ConnectionError:
         Popup("Error Connecting to Database.",
               "Please try again or restart the program")
-
+    except SQLErrors.ProgrammingError:
+        PopupError("ERROR: Invalid credentials.", "Try again")
     cursor.close()
 
 
@@ -126,7 +136,7 @@ def get_income_and_expense(user: str) -> Tuple[float, float]:
         SELECT SUM(amount)
         FROM transactions
         WHERE
-        exp_date BETWEEN '{year}-{month}-01' AND '{year}-{month}-31'
+        exp_date BETWEEN '{year}-{month}-01' AND '{year}-{month}-{days_in_month}'
         AND exp_type = 'CR'
         AND username = '{user}';""")
 
@@ -142,7 +152,7 @@ def get_income_and_expense(user: str) -> Tuple[float, float]:
         SELECT SUM(amount)
         FROM transactions
         WHERE
-        exp_date BETWEEN '{year}-{month}-01' AND '{year}-{month}-31'
+        exp_date BETWEEN '{year}-{month}-01' AND '{year}-{month}-{days_in_month}'
         AND exp_type = 'DR'
         AND username = '{user}';""")
 
@@ -243,7 +253,7 @@ def get_transactions(user: Union[str, int],
     transactions: List[Tuple] = cursor.fetchall()
     print(transactions)
     trans_table = Table(transactions, headings, key="table", right_click_menu=["Options", [
-                        "Edit", "Delete"]], enable_events=True) if number_of_records != 0 else T("No records to display")
+                        "Edit", "Delete"]], enable_events=True) if number_of_records != 0 else Table(["No records to display"], headings=[" "*50], key="table")
 
     return transactions, trans_table, number_of_records
 
@@ -251,73 +261,100 @@ def get_transactions(user: Union[str, int],
 # <-------------------- PyPlot -------------------------> #
 def get_graph_values(start_date: str = f"{year}-{month}-1",
                      end_date: str = f"{year}-{month}-{day}",
-                     exp_type: str = "Credit"
+                     exp_type: str = "All"
                      ):
+
+    global graph_active
 
     cursor = conn.cursor()
     q_cr = f"""
-    SELECT particulars,amount 
-    FROM transactions 
-    WHERE 
-    exp_date BETWEEN "{start_date}" AND "{end_date}" 
-    AND exp_type = "CR";
+    SELECT particulars,amount,DAY(exp_date)
+    FROM transactions
+    WHERE
+    exp_date BETWEEN "{start_date}" AND "{end_date}"
+    AND exp_type = "CR"
+    ORDER BY exp_date;
     """
 
     q_dr = f"""
-    SELECT particulars,amount 
-    FROM transactions 
-    WHERE 
-    exp_date BETWEEN "{start_date}" AND "{end_date}" 
-    AND exp_type = "DR";
+    SELECT particulars,amount,DAY(exp_date)
+    FROM transactions
+    WHERE
+    exp_date BETWEEN "{start_date}" AND "{end_date}"
+    AND exp_type = "DR"
+    ORDER BY exp_date;
     """
 
-    q_all = f"""
-    SELECT particulars,amount 
-    FROM transactions 
-    WHERE 
-    exp_date BETWEEN "{start_date}" AND "{end_date}";
-    """
+    def plot_graphs():
+        if exp_type == 'Credit':
+            q = q_cr
+        elif exp_type == 'Debit':
+            q = q_dr
+        elif exp_type == 'All':
+            q1 = q_cr
+            q2 = q_dr
+        
+        x = np.arange(1, days_in_month)
 
-    if exp_type == 'Credit':
-        q = q_cr
-    elif exp_type == 'Debit':
-        q = q_dr
-    elif exp_type == 'Both':
-        q1 = q_cr
-        q2 = q_dr
+        plt.xticks(np.arange(1,days_in_month+1),range(1,days_in_month+1))
+
+        if exp_type in ("Credit", "Debit"):
+            cursor.execute(q)
+            points = cursor.fetchall()
+            
+            x = np.array([point[2] for point in points])
+            y = np.array([point[1] for point in points])
+            
+            plt.plot(x, y, marker = "o",label=exp_type)
+
+            plt.grid(True)
+        else:
+            # <------- Credit -------> #
+            cursor.execute(q1)
+            points_1 = cursor.fetchall()
+            x1 = np.array([point[2] for point in points_1])  # Dates
+            y1 = np.array([point[1] for point in points_1])  # Amount
+
+            cursor.reset()
+
+            # <------- Debit -------> #
+            cursor.execute(q2)
+            points_2 = cursor.fetchall()
+
+            x2 = np.array([point[2] for point in points_2])
+            y2 = np.array([point[1] for point in points_2])
+
+            plt.plot(x1, y1, marker="o", label="Credit")
+            plt.plot(x2, y2, marker="x", label="Debit")
+
+            plt.grid(True)
+
+        plt.title(f"Report for the month of {month_name}-{year}")
+        plt.legend()
+
+        fig = plt.gcf()  # gcf -> get current figure #
+        fig_x, fig_y, fig_w, fig_h = fig.bbox.bounds
+        return fig, fig_w, fig_h
+    # q_all = f"""
+    # SELECT particulars,amount,DAY(exp_date) 
+    # FROM transactions 
+    # WHERE 
+    # exp_date BETWEEN "{start_date}" AND "{end_date}" 
+    # ORDER BY exp_date;
+    # """
+    if not graph_active:
+        return plot_graphs()
 
     else:
-        q = q_all
+        # plt.clf()
 
-    if exp_type in ("Credit", "Debit", "All"):
-        cursor.execute(q)
-        points = cursor.fetchall()
-        x = np.arange(0, len(points))
-        y = [point[1] for point in points]
-        plt.plot(x, y)
-    else:
-        cursor.execute(q1)
-        points_1 = cursor.fetchall()
-        x1 = np.arange(0, len(points_1))
-        y1 = [point[1] for point in points_1]
+        return plot_graphs()
+    # graph_active = True
 
-        cursor.reset()
-
-        cursor.execute(q2)
-        points_2 = cursor.fetchall()
-        x2 = np.arange(0, len(points_1))
-        y2 = [point[1] for point in points_2]
-
-        plt.plot(x1, y1)
-        plt.plot(x2, y2)
-        plt.grid(True)
-
-    fig = plt.gcf()
-    fig_x, fig_y, fig_w, fig_h = fig.bbox.bounds
-
-    return fig, fig_w, fig_h
+    cursor.close()
 
 # <------------------------------- Beginning of Matplotlib helper code -----------------------> #
+# <----- Taken from https://github.com/PySimpleGUI/PySimpleGUI/blob/master/DemoPrograms/Demo_Matplotlib.py -----> #
 
 
 def draw_figure(canvas, figure, loc=(0, 0)):
@@ -325,6 +362,12 @@ def draw_figure(canvas, figure, loc=(0, 0)):
     figure_canvas_agg.draw()
     figure_canvas_agg.get_tk_widget().pack(side='top', fill='both', expand=1)
     return figure_canvas_agg
+
+
+def delete_figure_agg(figure_agg):
+    figure_agg.get_tk_widget().forget()
+    plt.close('all')
+
 # <----------------------- GUI -----------------------------> #
 
 #   ________  ___  ___  ___
@@ -348,9 +391,9 @@ enjoyable and painless.
 
 
 class Xpnsit:
+    
     def __init__(self):
         self.app_state: bool = True
-
     # <------------------- Misc. Functions (Layouts and Updaters and stuff) --------------------> #
 
     def Add_Trans(self, particulars: str, _type: str, amount: float, date: str):
@@ -407,18 +450,18 @@ class Xpnsit:
         history_values, table, no_of_records = get_transactions(
             self.user.uname)
 
+        self.slider = sg.Slider(
+            range=(0, no_of_records),
+            default_value=no_of_records,
+            orientation='h',
+            enable_events=True,
+            key='slider'
+        )
+
         layout = [
             [T("Transaction History", font=("Helvetica", 18))],
             [T("All your transactions, in one place. Right click any one to delete or edit it.")],
-            [sg.T('Number of records to be shown:'),
-             sg.Slider(
-                 range=(0, no_of_records),
-                 default_value=no_of_records,
-                 orientation='h',
-                 enable_events=True,
-                 key='slider'
-            )
-            ],
+            [T('Number of records to be shown:'), self.slider],
             [T("Show records from "),
              Input(f"{year}-{month}-1", key="start_date", size=(10, 1)),
              CalendarButton("Start date", target="start_date", default_date_m_d_y=(
@@ -445,19 +488,36 @@ class Xpnsit:
         start, end = self.values['start_date'], self.values["end_date"]
         aod = 'ASC' if self.values["asc_or_desc"] == "Ascending" else "DESC"
         sort = "particulars" if self.values["sort_by"] == "Name" else "amount" if self.values["sort_by"] == "Amount" else "exp_date"
-
+        n = self.values["slider"] if self.event == 'slider' else 10000
         new_trans, new_table, new_number_of_trans = get_transactions(
             self.user.user_id,
-            int(self.values["slider"]),
+            int(n),
             start,
             end,
-            aod,
+            aod,  # a(scending)o(r)d(escending)
             sort
         )
         print(new_trans, new_table, new_number_of_trans)
-        self.win["table"].Update(new_trans)
-        self.win["slider"].Update(range=(0, new_number_of_trans))
+
+        self.win["table"].Update(new_trans)  # Updates table
+
+        # Updates max number of records to be possibly displayed
+        self.win["slider"].Update(range=(0, new_number_of_trans+1))
+
+        # Updates the default value of the slider to be the max
+        self.slider.Update(value=new_number_of_trans)
+
         self.win.Refresh()
+
+    def create_graph(self):
+
+        fig, w, h = get_graph_values(
+            self.values['a_start_date'],
+            self.values['a_end_date'],
+            self.values["a_type"],
+        )
+        self.figure_agg = draw_figure(
+            self.win['canvas'].TKCanvas, fig)
     # <------------------ Main Screens --------------------> #
 
     def Login(self):
@@ -513,7 +573,6 @@ class Xpnsit:
              Input(size=(35, 1), key="user")],
             [T("Password:", justification='center'), Input(
                 size=(35, 1), key="pass", password_char="*")],
-            [T("E-mail (Optional):", justification='center'), Input(key="mail")],
             [],
             [T(' '*40), Submit()]
         ]
@@ -562,11 +621,11 @@ class Xpnsit:
         else:
             dash_layout = [
                 [T(f"Welcome {self.user.first_name}")],
-                [T(f"Your expenses for {months[month-1]}-{year} are:"),
+                [T(f"Your expenses for {month_name}-{year} are:"),
                  T(str(expenses), font=("Arial", 20))],
-                [T(f"Your income for {months[month]}-{year} is:"),
+                [T(f"Your income for {month_name}-{year} is:"),
                  T(str(income), font=("Arial", 20))],
-                [T("-"*40, text_color="gray")],
+                [T("-"*80, text_color="gray")],
                 [T("Net Profit/Loss:", font=("Segoe", 18)),
                  T(str(income-expenses), font=("Arial", 24))]
             ]
@@ -591,6 +650,7 @@ class Xpnsit:
     def Analytics(self):
         fig, w, h = get_graph_values()
 
+
         analysis_layout = [
             [T("Analytics", font=("Helvetica", 18))],
             [T("Here you can find and generate graphs for your desired timeframe\nand observe trends in your balance.")],
@@ -603,7 +663,7 @@ class Xpnsit:
              CalendarButton("End date", target="a_end_date", default_date_m_d_y=(
                  month, day, year), button_color=("white", "red"), format="%Y-%m-%d")
              ],
-            [T("Type:"), Combo(["All", "Credit", "Debit", "Both"],
+            [T("Type:"), Combo(["All", "Credit", "Debit"],
                                default_value="All", key="a_type", readonly=True)],
             [Button("Generate", button_color=("white", "orange"))],
             [Canvas(size=(w, h), key="canvas")]
@@ -612,6 +672,9 @@ class Xpnsit:
         return analysis_layout
 
     def Interface(self):
+        global graph_active
+
+
         layout = [
             [T("Xpnsit", **heading_format), T(" "*50), Button("Settings"),
              Button("Log Out", button_color=("black", "yellow"))],
@@ -627,28 +690,30 @@ class Xpnsit:
             ],)]
         ]
 
-        self.win = Window("Xpnsit v1.0", layout=layout)
+        self.win = Window("Xpnsit v1.0", layout=layout, size = (590,640),resizable=True)
         while True:
             self.event, self.values = self.win.Read()
-
-            if self.event in (None, 'Exit'):
-                self.win.close()
-                self.app_state = False
-                break
-
-            if self.event != sg.TIMEOUT_KEY:
-                print(f"Event = {self.event}\nValues = {self.values}")
+            self.figure_agg = self.create_graph()
 
             if self.event == "Log Out":
-                logout = PopupYesNo("Are you sure?")
+                logout = PopupYesNo("Are you sure you want to log out?")
 
                 if logout == 'Yes':
                     sg.popup_quick_message(
-                        "Okay, closing. Bye", auto_close_duration=4)
+                        "Okay, closing. Bye", auto_close_duration=10)
                     self.win.close()
                     # self.app_state = False
                     del self.win
                     break
+            elif self.event is None:
+                self.win.close()
+                self.app_state = False
+                del self.win
+                break
+
+            if self.event != sg.TIMEOUT_KEY:
+                print(f"Event = {self.event}\nValues = {self.values}\n")
+
 
             if self.event == "Submit":
                 _type = "CR" if self.values["new_type"] in (
@@ -665,12 +730,20 @@ class Xpnsit:
                 self.win.refresh()
 
             if self.event == "Generate":
-                fig, w, h = get_graph_values(
-                    self.values['a_start_date'],
-                    self.values['a_end_date'],
-                    self.values["a_type"],
-                    )
-                figure_canvas_agg = draw_figure(self.win['canvas'].TKCanvas, fig)
+                # self.create_graph()
+                delete_figure_agg(self.figure_agg)
+                self.create_graph()
+                self.win.read()
+                self.win.refresh()
+            # elif self.event == "Generate" and self.figure_agg:
+                # canvas = self.win['canvas'].TKCanvas
+
+                
+
+                # canvas.delete("all")
+                # self.win.refresh()
+
+
 
 class NewUser:
     def __init__(self, *details):
